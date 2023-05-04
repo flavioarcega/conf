16face979ed018ee89a645c31a9efee3934ad028
Flávio Casas de Arcega <flavio@arcega.com.br>
2023-05-04 11:17:50 -0300

    Neovim on ubuntu

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -2,6 +2,8 @@ return {
   "williamboman/mason-lspconfig.nvim",
   opts = {
     ensure_installed = {
+      "diagnosticls",
+      "bashls",
       "lua_ls",
       -- web
       "html",
@@ -11,6 +13,9 @@ return {
       -- python
       "pylint",
       "jedi_language_server",
+      -- docker
+      "dockerls",
+      "docker_compose_language_service",
     },
   },
 }
